6dab0877a097d47b088665b575f5359cd64b34db
Anders Muskens <andersmuskens@gmail.com>
2013-04-04 09:54:28 -0600

    Added city class to be used as part of the economic system. Note cities are separate from network nodes

---
+++ economic.py
@@ -0,0 +1,19 @@
+# This class is for the game economic system. 
+# This system determines supply and demand of cities in 
+# the network. It also can update.
+
+# NOTE: THIS CLASS IS CURRENTLY A STUB
+
+class Economic():
+
+    def __init__(self):
+        self.cities = []
+
+    def Update(self):
+        # This function is called once per step. It updates the system.
+        pass
+
+    def GetDemand(self,x,y):
+        # Given a coordinate point, this function returns the demand
+        # for bandwidth at the location. 
+        return None
