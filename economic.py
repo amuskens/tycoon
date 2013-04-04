ce0ac7535a07132217cc860df8afed53ccf3df1f
Anders Muskens <andersmuskens@gmail.com>
2013-04-04 10:07:21 -0600

    Added simple functions to economic class that need to be expanded.

--- economic.py
+++ economic.py
@@ -4,16 +4,32 @@
 
 # NOTE: THIS CLASS IS CURRENTLY A STUB
 
+import math
+import random
+
+from city import *
+
 class Economic():
 
-    def __init__(self):
-        self.cities = []
+    def __init__(self,cities_list):
+        # Cities list is a list of 
+        # City objects that are part of the economic system.
+        self.cities = cities_list
 
+    # Accessors:
+    def GetCities(self):
+        return self.cities
+
+    # Step functions
     def Update(self):
         # This function is called once per step. It updates the system.
-        pass
+        for city in self.cities:
+            city.Update()
 
     def GetDemand(self,x,y):
         # Given a coordinate point, this function returns the demand
         # for bandwidth at the location. 
+
+        # It should find the nearest population center, then subtract  a "fade factor"
+        # depending on the distnace from the popualtion center. Shouldn't be negative.
         return None
